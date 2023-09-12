7fb82bd8945a6787e65a1f97229f6ca3fb1a9033
manku19978 <mayanksingh1997@gmail.com>
2023-09-12 17:19:06 +0530

    router, loader, style variables added

---
+++ src/components/modules/Layout/Layout.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./Layout.scss";
+import Loader from "../Loader/Loader";
+
+const Layout = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const navigate = useNavigate();
+
+  const redirectHandler = () => {
+    if (isAuthenticated) navigate("/library");
+    else navigate("/login");
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      redirectHandler();
+    }, 3000);
+  }, []);
+
+  return (
+    <div className="wrapper flex justify-center align-center">
+      <Loader />
+    </div>
+  );
+};
+
+export default Layout;
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LoginModal from "./components/modules/LoginModal/LoginModal";
+import Library from "./components/modules/Library/Library";
+import Layout from "./components/modules/Layout/Layout";
 import "./index.scss";
-import App from "./App";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+  },
+  {
+    path: "/login",
+    element: <LoginModal />,
+  },
+  {
+    path: "/library",
+    element: <Library />,
+  },
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
